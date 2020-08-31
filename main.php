31e6177c68dc2c6c95a8f7953bf0c5ec9be06c22
Shivam Tayal <41532647+Shivam1950@users.noreply.github.com>
2020-08-31 22:17:59 +0530

    Update main.php

--- main.php
+++ main.php
@@ -2,19 +2,17 @@
 
     include('config\db_connect.php');
 
-	// write query for all pizzas
 	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
-	// get the result set (set of rows)
+
 	$result = mysqli_query($conn, $sql);
 
-	// fetch the resulting rows as an array
 	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-	// free the $result from memory (good practise)
+
 	mysqli_free_result($result);
 
-	// close connection
+	
 	mysqli_close($conn);
 
 ?>
